0acc39a704365afffae03d64ea8b1f318be1c79a
Lars Kuhtz <lars@kadena.io>
2023-12-03 14:06:47 -0800

    improve query tool

--- tools/Query.js
+++ tools/Query.js
@@ -3,11 +3,11 @@
 // npm install
 // node ./tools/Header.js /db/0_old_rocksdb 3 4294600 20
 
-const { program } = require('commander');
+const { program, Command } = require('commander');
 const fs = require('fs');
 
 const cdb = require("../src/ChainDb");
-const { asyncIter } = require('../src/Utils');
+const { asyncIter, length } = require('../src/Utils');
 const { SHA256Hash } = require("../src/BlockHeader");
 
 const { createLogger, transports } = require('winston');
@@ -35,6 +35,30 @@ async function withReadOnlyDb (dbPath, fn) {
   }
 }
 
+function makeSizeCommand () {
+  const sizeCmd = new Command('approximate-size');
+  sizeCmd
+    .alias('size')
+    .description('approximate size in bytes of the table.')
+    .action(async (_, cmd) => {
+      const size = await cmd.tbl.approximateSize();
+      console.log(size);
+    });
+  return sizeCmd;
+}
+
+function makeCountCommand () {
+  const sizeCmd = new Command('count');
+  sizeCmd
+    .alias('size')
+    .description('number of entries in the table')
+    .action(async (_, cmd) => {
+      const it = cmd.tbl.iterator()
+      console.log(await asyncIter.length(it));
+    });
+  return sizeCmd;
+}
+
 /* By default Stringify BigInt values as strings
  */
 BigInt.prototype.toJSON = function() {
@@ -48,91 +72,205 @@ program
   .name("query-chainweb-db")
   .description("Return items from the Chainweb RocksDb")
   .version("0.0.0")
+
+  // provide database
   .option('-d, --database-directory <string>', "Chainweb RocksDb directory.", "/db/0/rocksDb")
-  .option('--debug', "enable debugging");
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    const dbDir = thisCmd.opts().databaseDirectory;
+    if (!fs.existsSync(dbDir)) {
+      logger.error(`failed to open database directory ${dbDir}`);
+      process.exit(1);
+    }
+    const db = new cdb.ChainDb(dbDir);
+    await db.open({ readOnly: true });
+    logger.debug("db opened in readonly mode");
+    actionCmd.db = db;
+  })
+  .hook('postAction', async (thisCmd, actionCmd) => {
+    const db = actionCmd.db;
+    await db.close();
+    logger.debug("db closed");
+  })
+
+  // set loglevel
+  .option('--debug', "enable debugging")
+  .hook('preSubcommand', (thisCmd, subCmd) => {
+    logger.level = thisCmd.opts().debug ? 'debug' : 'warn';
+    logger.debug('Debugging enabled');
+  });
 
 /* ************************************************************************** */
-/* Print Headers */
+/* Header */
 
-async function headers (dbPath, chain, start, count) {
-  withReadOnlyDb(dbPath, async (db) => {
-    const hdr_tbl = db.headerTable(chain);
-    const it = hdr_tbl.iterator()
+const headerTable = program
+  .command('header')
+  .alias('hdr')
+  .requiredOption('-c, --chain-id <int>', "Id of the chain from which headers are returned.")
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    const chain = thisCmd.opts().chainId;
+    actionCmd.tbl = actionCmd.db.headerTable(chain);
+  });
+
+headerTable.command('list')
+  .description('list entries from the BlockHeader table')
+  .option('-s, --start-height <int>', "Start block height.", 0)
+  .option('-n, --count <int>', "Number of headers that are returned.", 1)
+  .option('--deps', "Include dependencies from the rank and payload tables)")
+  .option('--check', "Like --deps, but only checks whether dependencies exist.")
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    const chain = thisCmd.optsWithGlobals().chainId;
+    actionCmd.ranksTbl = actionCmd.db.rankTable(chain);
+    actionCmd.payloadTbl = actionCmd.db.payloadWithOutputsTable(chain);
+  })
+  .action(async (_, cmd) => {
+    logger.log('debug', { options: cmd.optsWithGlobals() });
+
+    const options = cmd.opts();
+    const start = BigInt(options.startHeight);
+    const count = options.count;
+
+    const it = cmd.tbl.iterator()
     try {
       it.seek({height: start, hash: null});
         const it2 = asyncIter.take(it, count);
         for await (let x of it2) {
-          console.log(`${JSON.stringify(x)}`);
+          if (cmd.opts().deps) {
+            const r = await cmd.ranksTbl.get(x.value.hash)
+              .catch(e => e.toString());
+            const p = await cmd.payloadTbl.get(x.value.payloadHash)
+              .catch(e => e.toString());
+            console.log(JSON.stringify({header: x, rank: r, payload: p}));
+          } else if (cmd.opts().check) {
+            const r = await cmd.ranksTbl.get(x.value.hash)
+              .then(_ => true)
+              .catch(_ => false);
+            const p = await cmd.payloadTbl.get(x.value.payloadHash)
+              .then(_ => true)
+              .catch(_ => false);
+            console.log(JSON.stringify({header: x, rank: r, payload: p}));
+          } else {
+            console.log(JSON.stringify(x));
+          }
         }
     } finally {
       it.end();
     }
   });
-}
 
-program.command('headers')
-  .description('print entries from the BlockHeader table')
-  .option('-c, --chain-id <int>', "Id of the chain from which headers are returned.", 0)
-  .option('-s, --start-height <int>', "Start block height.", 0)
-  .option('-n, --count <int>', "Number of headers that are returned.", 1)
-  .action((_, cmd) => {
+headerTable.addCommand(makeSizeCommand());
+headerTable.addCommand(makeCountCommand());
 
-    const options = cmd.optsWithGlobals();
-    logger.level = options.debug ? 'debug' : 'warn';
-    logger.log('debug', { options: options});
+/* ************************************************************************** */
+/* Payload */
 
-    const dbDir = options.databaseDirectory;
-    const chain = options.chainId;
+const payloadTable = program
+  .command('payload-with-outputs')
+  .alias("pwo")
+  .requiredOption('-c, --chain-id <int>', "Id of the chain from which payloads are returned.")
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    const chain = thisCmd.opts().chainId;
+    actionCmd.tbl = actionCmd.db.payloadWithOutputsTable(chain);
+  });
+
+payloadTable.command("get")
+  .description('get block block payload')
+  .requiredOption('-p, --payload-hash <string>', "The payload hash of the payload")
+  .action(async (_, cmd) => {
+    logger.log('debug', { options: cmd.optsWithGlobals() });
+    const options = cmd.opts();
+    const hash = new SHA256Hash(Buffer.from(options.payloadHash, 'base64url'));
+    const v = await cmd.tbl.get(hash);
+    console.log(JSON.stringify(v));
+  });
+
+payloadTable.addCommand(makeSizeCommand());
+payloadTable.addCommand(makeCountCommand());
+
+/* ************************************************************************** */
+/* Rank */
+
+const rankTable = program
+  .command('block-rank')
+  .alias('height')
+  .alias("rank")
+  .requiredOption('-c, --chain-id <int>', "Id of the chain from which payloads are returned.")
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    const chain = thisCmd.opts().chainId;
+    actionCmd.tbl = actionCmd.db.rankTable(chain);
+  });
+
+rankTable.command('get')
+  .description('get block height for a block hash')
+  .requiredOption('-b, --block-hash <string>', "The block hash for which the height is looked up.")
+  .action(async (_, cmd) => {
+    logger.log('debug', { options: cmd.optsWithGlobals() });
+    const options = cmd.opts();
+    const hash = new SHA256Hash(Buffer.from(options.blockHash, 'base64url'));
+    const v = await cmd.tbl.get(hash);
+    console.log(Number(v));
+  });
+
+rankTable.addCommand(makeSizeCommand());
+rankTable.addCommand(makeCountCommand());
+
+/* ************************************************************************** */
+/* Cut */
+
+const cutTable = program
+  .command('cut')
+  .hook('preAction', async (thisCmd, actionCmd) => {
+    actionCmd.tbl = actionCmd.db.cutTable;
+  });
+
+// TODO add block height start option
+cutTable.command('list')
+  .description('list entries from the cut table')
+  .option('-s, --start-height <int>', "Start cut height.", 0)
+  .option('-n, --count <int>', "Number of cuts that are returned.", 1)
+  // .option('--deps', "Include dependencies from the rank and payload tables)")
+  // .option('--check', "Like --deps, but only checks whether dependencies exist.")
+  // .hook('preAction', async (thisCmd, actionCmd) => {
+  //   const chain = thisCmd.optsWithGlobals().chainId;
+  //   actionCmd.ranksTbl = actionCmd.db.rankTable(chain);
+  //   actionCmd.payloadTbl = actionCmd.db.payloadWithOutputsTable(chain);
+  // })
+  .action(async (_, cmd) => {
+    logger.log('debug', { options: cmd.optsWithGlobals() });
+
+    const options = cmd.opts();
     const start = BigInt(options.startHeight);
     const count = options.count;
 
-    if (!fs.existsSync(dbDir)) {
-      logger.error(`failed to open database directory ${dbDir}`);
-      process.exit(1);
+    const it = cmd.tbl.iterator()
+    try {
+      it.seek({height: start, hash: null});
+        const it2 = asyncIter.take(it, count);
+        for await (let x of it2) {
+          // if (cmd.opts().deps) {
+          //   const r = await cmd.ranksTbl.get(x.value.hash)
+          //     .catch(e => e.toString());
+          //   const p = await cmd.payloadTbl.get(x.value.payloadHash)
+          //     .catch(e => e.toString());
+          //   console.log(JSON.stringify({header: x, rank: r, payload: p}));
+          // } else if (cmd.opts().check) {
+          //   const r = await cmd.ranksTbl.get(x.value.hash)
+          //     .then(_ => true)
+          //     .catch(_ => false);
+          //   const p = await cmd.payloadTbl.get(x.value.payloadHash)
+          //     .then(_ => true)
+          //     .catch(_ => false);
+          //   console.log(JSON.stringify({header: x, rank: r, payload: p}));
+          // } else {
+            console.log(JSON.stringify(x));
+          // }
+        }
+    } finally {
+      it.end();
     }
-
-    headers(dbDir, chain, start, count);
   });
 
-program.command('payload')
-  .description('print a block payload')
-  .option('-c, --chain-id <int>', "Id of the chain from which headers are returned.", 0)
-  .requiredOption('-p, --payload-hash <string>', "The payload hash of the payload")
-  .action((_, cmd) => {
-    const options = cmd.optsWithGlobals();
-    logger.level = options.debug ? 'debug' : 'warn';
-    logger.log('debug', { options: options});
-
-    const dbDir = options.databaseDirectory;
-    const chain = options.chainId;
-    const hash = new SHA256Hash(Buffer.from(options.payloadHash, 'base64url'));
-
-    withReadOnlyDb(dbDir, async (db) => {
-      const tbl = db.payloadWithOutputsTable(chain);
-      const v = await tbl.get(hash);
-      console.log(JSON.stringify(v));
-    });
-  });
-
-program.command('rank')
-  .description('find block height for a block hash')
-  .option('-c, --chain-id <int>', "Id of the chain.", 0)
-  .requiredOption('-b, --block-hash <string>', "The block hash for which the height is looked up.")
-  .action((_, cmd) => {
-    const options = cmd.optsWithGlobals();
-    logger.level = options.debug ? 'debug' : 'warn';
-    logger.log('debug', { options: options});
-
-    const dbDir = options.databaseDirectory;
-    const chain = options.chainId;
-    const hash = new SHA256Hash(Buffer.from(options.blockHash, 'base64url'));
-
-    withReadOnlyDb(dbDir, async (db) => {
-      const tbl = db.rankTable(chain);
-      const v = await tbl.get(hash);
-      console.log(Number(v));
-    });
-  });
+cutTable.addCommand(makeSizeCommand());
+cutTable.addCommand(makeCountCommand());
 
 /* ************************************************************************** */
 /* Main */
